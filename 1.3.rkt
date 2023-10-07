1e42872f8ace5e1470a714efd985fbdc84cc258d
kurku <sandovin@mail.ru>
2023-10-07 11:48:35 +0300

    first exercise

---
+++ 1.3.rkt
@@ -0,0 +1,13 @@
+#lang sicp
+(define (square x) (* x x))
+
+(define (sum-of-squares x y)
+  (+ (square x) (square y)))
+
+(define (f x y z)
+  (cond ((and (<= x y) (<= x z))
+          (sum-of-squares y z))
+        ((and (<= y x) (<= y z))
+          (sum-of-squares x z))
+        (else
+          (sum-of-squares x y))))
