886d3d3c0f52f700046cdf686c13ff6dc156e1e2
kurku <sandovin@mail.ru>
2023-10-08 14:11:02 +0300

    lexically scoped

--- 1.7.rkt
+++ 1.7.rkt
@@ -7,16 +7,16 @@
 (define (within x y eps)
   (< (abs (- x y)) eps))
 
-(define (sqrt-iter guess new-guess x)
-  (if (good-enough? guess new-guess)
-      guess
-      (sqrt-iter new-guess (improve new-guess x) x)))
-
-(define (improve guess x)
-  (average guess (/ x guess)))
-
-(define (good-enough? guess new-guess)
-  (within guess new-guess 0.001))
-
 (define (sqrt x)
-  (sqrt-iter 1.0 (/ x 2.0) x))
+  (define (sqrt-iter guess new-guess)
+    (if (good-enough? guess new-guess)
+        guess
+        (sqrt-iter new-guess (improve new-guess))))
+
+  (define (improve guess)
+    (average guess (/ x guess)))
+
+  (define (good-enough? guess new-guess)
+    (within guess new-guess 0.001))
+
+  (sqrt-iter 1.0 (/ x 2.0)))
