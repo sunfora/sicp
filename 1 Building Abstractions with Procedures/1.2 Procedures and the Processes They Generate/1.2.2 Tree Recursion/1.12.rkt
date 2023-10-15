8ed42554538dd62aecf98346f25b7cb716dfa860
kurku <sandovin@mail.ru>
2023-10-15 10:49:23 +0300

    pascal triangle reworked

--- 1 Building Abstractions with Procedures/1.2 Procedures and the Processes They Generate/1.2.2 Tree Recursion/1.12.rkt
+++ 1 Building Abstractions with Procedures/1.2 Procedures and the Processes They Generate/1.2.2 Tree Recursion/1.12.rkt
@@ -8,7 +8,19 @@
            (pt (dec row) (dec pos)))))
   (pt row pos))
 
-(display (pascal-triangle 1 1))  (newline) 
-(display (pascal-triangle 2 1))  (display (pascal-triangle 2 2)) (newline)
-(display (pascal-triangle 3 1))  (display (pascal-triangle 3 2)) (display (pascal-triangle 3 3)) (newline) 
-(display (pascal-triangle 4 1))  (display (pascal-triangle 4 2)) (display (pascal-triangle 4 3)) (display (pascal-triangle 4 4)) (newline)
+(define (display-pascal k)
+  (define (display-row k)
+    (define (iter i)
+      (if (<= i k)
+          (begin 
+            (display (pascal-triangle k i)) (display " ")
+            (iter (inc i)))))
+    (iter 1))
+  (define (iter i)
+    (if (<= i k)
+        (begin
+          (display-row i) (newline)
+          (iter (inc i)))))
+  (iter 1))
+
+(display-pascal 6)
