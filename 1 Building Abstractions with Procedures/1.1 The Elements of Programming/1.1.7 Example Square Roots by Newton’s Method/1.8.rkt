7f786f2378352b62c52c4d226867d930bbf109b2
sunfora <90210938+sunfora@users.noreply.github.com>
2023-10-27 18:54:24 +0300

    fixed 1.8

--- 1 Building Abstractions with Procedures/1.1 The Elements of Programming/1.1.7 Example Square Roots by Newton’s Method/1.8.rkt
+++ 1 Building Abstractions with Procedures/1.1 The Elements of Programming/1.1.7 Example Square Roots by Newton’s Method/1.8.rkt
@@ -2,19 +2,23 @@
 
 (define (square x) (* x x))
 
+(define (relative-error x y)
+  (/ (abs (- x y))
+     y))
+
+(define (good-enough? previous-guess guess)
+  (< (relative-error previous-guess guess) 0.001))
+
+(define (cbrt-iter previous-guess guess x)
+  (if (good-enough? previous-guess guess)
+      previous-guess
+      (cbrt-iter guess (improve guess x) x)))
+
+(define (improve y x)
+  (/ (+ (/ x (square y)) (* 2.0 y)) 3.0))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
 (define (cbrt x)
-  (define (within x y eps)
-    (< (abs (- x y)) eps))
-
-  (define (cbrt-iter guess new-guess)
-    (if (good-enough? guess new-guess)
-        guess
-        (cbrt-iter new-guess (improve new-guess))))
-
-  (define (good-enough? guess new-guess)
-    (within guess new-guess 0.001))
-
-  (define (improve y)
-    (/ (+ (/ x (square y)) (* 2.0 y)) 3.0))
-  
-  (cbrt-iter x (improve x)))
+  (cbrt-iter 1.0 (improve 1.0 x) x))
